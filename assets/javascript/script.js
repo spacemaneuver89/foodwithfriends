cc5f87ece5751a27c77f55be17ba0e4c951e1286
Erick Garcia <erickjgarcia@gmail.com>
2018-09-19 21:13:11 -0600

    deleted some useless code (#16)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,8 +13,6 @@ dbRef.on('value', snapshot => {
 	console.log(snapshot.val());
 });
 
-// function searchFilm() {
-// }
 
 
 
@@ -66,105 +64,10 @@ function searchFilmGenre() {
 
 				})
 
-
-			// $("#filmCard").html('<img class="card-img-top" src=' + poster + "alt='Card image cap' id='filmImage'> <div class='card-body'> <h5 class='card-title' id='filmTitle'> " + filmTitle + "</h5> <p class = 'card-text' id = 'filmText'>" + genre + "</p> <p>" + plot + "</p>");
-
-			for (i = 0; i < 3; i ++) {
-				var filmCard = $("<div class='card' style='width:25rem'>"); 
-
-				var imgURL = response.hits.recipe.image;
-				var cardImg = $("<img>").attr("src", imgURL);
-				
-				var cardTitle = response.hits.recipe.label;
-				var displayTitle = $("<p class='card-title'>").text(cardTitle);
-				
-				var cardText = response.hits.recipe.source;
-				var displayText = $("<p class='card-text'>").text(cardText); 
-
-				var cardLink = response.hits.recipe.url;
-				var displayLink = $("<p class='btn btn-danger'>").text(cardLink); 
-
-				filmCard.prepend(cardImg); 
-				filmCard.append(displayTitle);
-				filmCard.append(displayText);
-				filmCard.append(displayLink);
-			
-				$(".filmBio").prepend(filmCard);
-			}
-
-			for (i = 0; i < 3; i ++) {
-				var filmCard = $("<div class='card' style='width:25rem'>"); 
-
-				var imgURL = response.Poster;
-				var cardImg = $("<img>").attr("src", imgURL);
-				
-				var cardTitle = response.hits.recipe.label;
-				var displayTitle = $("<p class='card-title'>").text(cardTitle);
-				
-				var cardText = response.hits.recipe.source;
-				var displayText = $("<p class='card-text'>").text(cardText); 
-
-				var cardLink = response.hits.recipe.url;
-				var displayLink = $("<p class='btn btn-danger'>").text(cardLink); 
-
-				filmCard.prepend(cardImg); 
-				filmCard.append(displayTitle);
-				filmCard.append(displayText);
-				filmCard.append(displayLink);
-			
-				$(".filmBio").prepend(filmCard);
-			}
-
-
-		})
-	})
-};
-
-// $('.dropdown-menu').dropdown('toggle');
-
 searchFilmGenre();
 
-// findRecipesByFoodType();
-
-// searchRecipes();
-
-// function findRecipesByFoodType() {
 
 
-	$('#findFoodBtn').on('click', function () {
-		event.preventDefault();
-		var test = $("#selectNumber").val();
-		console.log(test);
-		console.log(myobject);
-		console.log("button works");
-		console.log(myobject.Value1);
-		console.log(select.options.text);
-
-	});
-}
-
-function searchRecipes() {
-	$("#findRecipeBtn").on("click", function () {
-		event.preventDefault();
-		var recipe = $("#recipeInput").val();
-		console.log(recipe);
-		var recipeQuery = "https://api.edamam.com/search?q=" + recipe + "&app_id=12348d3a&app_key=6579ebfed3d6935657e6dccc1c8514bc&from=0&to=3"
-		console.log(recipeQuery);
-		$.ajax({
-			url: recipeQuery,
-			method: "GET"
-		}).then(function (response2) {
-			console.log(response2);
-
-			var recipePoster = response2.hits[0].recipe.image;
-			console.log(recipePoster);
-
-			$("#foodBio").html('<img class="card-img-top" src=' + recipePoster + "alt='Card image cap' id='foodImage'> <div class='card-body'> <h5 class='card-title' id='foodTitle'> " + x + "</h5> <p class = 'card-text' id = 'foodText'>" + y + "</p>");
-
-
-// 		})
-// 	})
-// }
 
 
 
